a53a465cecec7f0bac50fdbded43bb84791a49ca
pradhumnC1292 <chandrawatpradhumn1292@gmail.com>
2024-08-31 22:04:46 +0530

    second commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for all routes
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://vivansh-project-001.netlify.app/",
     credentials: true, // Allow cookies to be sent with requests
   })
 );
